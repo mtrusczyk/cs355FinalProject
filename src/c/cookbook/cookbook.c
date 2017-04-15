820cb7af75c4e1da35b8fbc327bf928e6223a849
Matthew Rusczyk <mtrusczyk@gmail.com>
2017-04-15 14:12:52 -0400

    First NFC implimentation

--- src/c/cookbook/cookbook.c
+++ src/c/cookbook/cookbook.c
@@ -192,3 +192,71 @@ void load_recipes(char dirname[])
 		closedir(dir_ptr);
 	}
 }
+
+#include <stdlib.h>
+#include <nfc/nfc.h>
+
+static void
+print_hex(const uint8_t *pbtData, const size_t szBytes)
+{
+  size_t  szPos;
+
+  for (szPos = 0; szPos < szBytes; szPos++) {
+    printf("%02x  ", pbtData[szPos]);
+  }
+  printf("\n");
+}
+
+
+char *getUID(int argc, const char *argv[])
+{
+  nfc_device *pnd;
+  nfc_target nt;
+  char UID[8];
+
+  // Allocate only a pointer to nfc_context
+  nfc_context *context;
+
+  // Initialize libnfc and set the nfc_context
+  nfc_init(&context);
+  if (context == NULL) {
+    printf("Unable to init libnfc (malloc)\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Display libnfc version
+  const char *acLibnfcVersion = nfc_version();
+  (void)argc;
+
+  // Open, using the first available NFC device which can be in order of selection:
+  //   - default device specified using environment variable or
+  //   - first specified device in libnfc.conf (/etc/nfc) or
+  //   - first specified device in device-configuration directory (/etc/nfc/devices.d) or
+  //   - first auto-detected (if feature is not disabled in libnfc.conf) device
+  pnd = nfc_open(context, NULL);
+
+  if (pnd == NULL) {
+    printf("ERROR: %s\n", "Unable to open NFC device.");
+    exit(EXIT_FAILURE);
+  }
+  // Set opened NFC device to initiator mode
+  if (nfc_initiator_init(pnd) < 0) {
+    nfc_perror(pnd, "nfc_initiator_init");
+    exit(EXIT_FAILURE);
+  }
+
+  // Poll for a ISO14443A (MIFARE) tag
+  const nfc_modulation nmMifare = {
+    .nmt = NMT_ISO14443A,
+    .nbr = NBR_106,
+  };
+  if (nfc_initiator_select_passive_target(pnd, nmMifare, NULL, 0, &nt) > 0) {
+    printf("       UID (NFCID%c): ", (nt.nti.nai.abtUid[0] == 0x08 ? '3' : '1'));
+    get_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen, UID);
+  }
+  // Close NFC device
+  nfc_close(pnd);
+  // Release the context
+  nfc_exit(context);
+  return UID;
+}
